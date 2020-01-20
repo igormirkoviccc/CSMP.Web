30c560c76b3876395b9ad4649d447afe3713ce34
Igor <igor.mirkovicc@gmail.com>
2020-01-20 22:58:36 +0100

    adding ref to rect

--- src/components/context/Canvas.js
+++ src/components/context/Canvas.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { Stage, Layer, Rect} from 'react-konva';
+import { Stage, Layer, Rect, Group, Text} from 'react-konva';
 import Konva from 'konva';
 import Operation from "../Operation";
 
@@ -8,25 +8,40 @@ class Canvas extends Component{
         super(props);
         this.state = {
 
+
         }
+        this.myRef = React.createRef();
+    }
+
+
+
+    check = ()=>{
+
     }
 
     render(){
+        console.log(this.myRef.current);
         return (
             <div className='canvas_context'>
                 <Stage width={window.innerWidth - this.props.resizableWidth - 100} height={window.innerHeight}>
                     <Layer>
                         {this.props.currentItems.map((item,index) =>{
-                            return <Rect onClick={() => {console.log('hello im '+item)}}
-                                x={100 + index * 100}
-                                y={100}
-                                draggable
-                                width={50}
-                                height={50}
-                                fill={'#86cfca'}
-                                borderRadius={7}
-                                className='operation_container'
-                            />
+                            return (<div>
+                                <Rect
+                                    onClick={this.check}
+                                    x={100 + index * 100}
+                                    y={100}
+                                    draggable
+                                    width={50}
+                                    height={50}
+                                    fill={'#86cfca'}
+                                    borderRadius={7}
+                                    className='operation_container'
+                                    ref={this.myRef}
+                                    data={item}
+                                />
+                            </div>)
+
                         })}
                     </Layer>
                 </Stage>
